4c7371219945a28434a4e4567bde60600c35c0ac
Matthew Napoli <123284603+matthewnapoli@users.noreply.github.com>
2023-07-31 05:32:36 -0700

    new

--- script.js
+++ script.js
@@ -1,94 +1,70 @@
-// Function to set up the typewriter effect
 function setupTypewriter(t) {
-    var HTML = t.innerHTML;
-  
-    t.innerHTML = "";
-  
-    var cursorPosition = 0,
-        tag = "",
-        writingTag = false,
-        tagOpen = false,
-        typeSpeed = 25,
-        tempTypeSpeed = 0;
-  
-    var type = function() {
+  var HTML = t.innerHTML;
+
+  t.innerHTML = "";
+
+  var cursorPosition = 0,
+      tag = "",
+      writingTag = false,
+      tagOpen = false,
+      typeSpeed = 15,
+    tempTypeSpeed = 0;
+
+  var type = function() {
+    
       if (writingTag === true) {
-        tag += HTML[cursorPosition];
-      }
-  
-      if (HTML[cursorPosition] === "<") {
-        tempTypeSpeed = 0;
-        if (tagOpen) {
-          tagOpen = false;
-          writingTag = true;
-        } else {
-          tag = "";
-          tagOpen = true;
-          writingTag = true;
           tag += HTML[cursorPosition];
-        }
-      }
-  
-      if (!writingTag && tagOpen && HTML[cursorPosition] == "&") {
-        tag.innerHTML += "&";
-        cursorPosition+=4
       }
 
-      if (!writingTag && tagOpen && HTML[cursorPosition] != "&") {
-        tag.innerHTML += HTML[cursorPosition]
-      }
-
-
-  
-      if (!writingTag && !tagOpen) {
-        if (HTML[cursorPosition] === " ") {
+      if (HTML[cursorPosition] === "<") {
           tempTypeSpeed = 0;
-        } else {
-          tempTypeSpeed = typeSpeed;
-        }
-        t.innerHTML += HTML[cursorPosition];
+          if (tagOpen) {
+              tagOpen = false;
+              writingTag = true;
+          } else {
+              tag = "";
+              tagOpen = true;
+              writingTag = true;
+              tag += HTML[cursorPosition];
+          }
       }
-  
+      if (!writingTag && tagOpen) {
+          tag.innerHTML += HTML[cursorPosition];
+      }
+      if (!writingTag && !tagOpen) {
+          if (HTML[cursorPosition] === " ") {
+              tempTypeSpeed = 0;
+          }
+          else {
+              tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+          }
+          t.innerHTML += HTML[cursorPosition];
+      }
       if (writingTag === true && HTML[cursorPosition] === ">") {
-        tempTypeSpeed = typeSpeed;
-        writingTag = false;
-        if (tagOpen) {
-          var newSpan = document.createElement("span");
-          t.appendChild(newSpan);
-          newSpan.innerHTML = tag;
-          tag = newSpan.firstChild;
-        }
+          tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+          writingTag = false;
+          if (tagOpen) {
+              var newSpan = document.createElement("span");
+              t.appendChild(newSpan);
+              newSpan.innerHTML = tag;
+              tag = newSpan.firstChild;
+          }
       }
-  
+
       cursorPosition += 1;
       if (cursorPosition < HTML.length - 1) {
-        setTimeout(type, tempTypeSpeed);
+          setTimeout(type, tempTypeSpeed);
       }
-    };
-  
-    return {
-      type: type,
-    };
-  }
-  
-  // Function to handle the image replacement
-  function replaceImage() {
-    var imagePlaceholder = document.querySelector(".image-placeholder");
-    var uclaImage = document.createElement("img");
-    uclaImage.src = "img/bruin-logo.png";
-    uclaImage.alt = "UCLA Bruins Logo";
-    uclaImage.style.width = "24px";
-    uclaImage.style.height = "24px";
-    uclaImage.style.marginBottom = "-4px";
-    imagePlaceholder.appendChild(uclaImage);
-  }
-  
-  // Entry point: set up typewriter and replace the image
-  document.addEventListener("DOMContentLoaded", function () {
-    var typer = document.getElementById("typewriter");
-    var typewriter = setupTypewriter(typer);
-    typewriter.type();
-  
-    replaceImage();
-  });
-  
+
+  };
+
+  return {
+      type: type
+  };
+}
+
+var typer = document.getElementById('typewriter');
+
+typewriter = setupTypewriter(typewriter);
+
+typewriter.type();
